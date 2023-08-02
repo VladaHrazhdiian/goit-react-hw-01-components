9de4872339bee7ff85b591559ea53c1ff0d1872d
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-08-03 00:40:34 +0300

    work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
+import Profile from "./Profile/Profile";
+
+import user from '../data/user.json';
+
+export function App() {
+  document.title = 'Homework 1';
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+          </>
   );
-};
+}
